460469b9ca6f2775e210154403bdd41a180100f1
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-08-24 19:41:32 -0400

    Adding Sitemap
    Steps:
    [1] - Install astrojs/sitemap via yarn install.
    [2] - Integrity check -> package / yarn.lock.
    [3] - Custom sitemap inclusions (i.e app.kbve.com).
    Test Case:
    [&] - Does the pipeline continue to function.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import sitemap from "@astrojs/sitemap";
+
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+	site: 'https://kbve.com',
+	
+  integrations: [
+	preact(), 
+	sitemap({
+	customPages: ['https://app.kbve.com/#/', 'https://app.kbve.com/#/asset/']
+  	})
+	]
 });
