dc754d5debaae08f034ab9a87e0da488406cfc51
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-08-28 01:13:02 -0400

    Pipeline Test .mdx File
    Converting some of the .md files into .mdx and now testing the pipeline.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://kbve.com',
-	
-  integrations: [
-	preact(), 
-	sitemap({
-	customPages: ['https://app.kbve.com/#/', 'https://app.kbve.com/#/asset/']
-  	})
-	]
+  site: 'https://kbve.com',
+  markdown: {
+    extendDefaultPlugins: true
+  },
+  integrations: [preact(), sitemap({
+    customPages: ['https://app.kbve.com/#/', 'https://app.kbve.com/#/asset/']
+  }), mdx()]
 });
