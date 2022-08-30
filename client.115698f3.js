982ee063f0a62e181476cb74299126d3b8146474
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-08-30 23:45:31 +0000

    Update gh-pages to output generated at a867450

---
+++ client.115698f3.js
@@ -0,0 +1,1 @@
+import{h as e,P as f}from"./chunks/preact.module.f099146f.js";const r=({value:t,name:n})=>t?e("astro-slot",{name:n,dangerouslySetInnerHTML:{__html:t}}):null;r.shouldComponentUpdate=()=>!1;const m=t=>(n,s,{default:o,...l})=>{if(!!t.hasAttribute("ssr")){for(const[a,u]of Object.entries(l))s[a]=e(r,{value:u,name:a});f(e(n,s,o!=null?e(r,{value:o}):o),t)}};export{m as default};
