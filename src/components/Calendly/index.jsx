77c472a2ba10727ced418352a5a6b153c54c0f69
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-09-09 16:39:12 -0400

    Updates to the css
    It seems that H3 can not be read.

--- src/components/Calendly/index.jsx
+++ src/components/Calendly/index.jsx
@@ -1,5 +1,5 @@
 import { h } from 'preact';
-const Calendly = ({user = 'kbve', width = 320, height = 630, display = true, hide = 0}) => {
+const Calendly = ({user = 'kbve', width = 320, height = 630, display = true, hide = 0, text_color="", primary_color=""}) => {
 
     if(display)
 	return (
@@ -7,7 +7,7 @@ const Calendly = ({user = 'kbve', width = 320, height = 630, display = true, hid
                 <div class="calendly-inline-widget" data-url={
                     `https://calendly.com/`
                     + user +
-                    `?hide_landing_page_details=`+hide+`&hide_gdpr_banner=`+hide
+                    `?hide_landing_page_details=`+hide+`&hide_gdpr_banner=`+hide+`&text_color=`+text_color+`&primary_color=`+primary_color
                 } 
                     style={
                         `min-width:`+width+`px;height:`+height+`px;`
