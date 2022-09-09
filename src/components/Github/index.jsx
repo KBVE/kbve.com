ff39abf33a37c0745a99d56f86bd203387e11918
J. Zane Cook <90814067+jzanecook@users.noreply.github.com>
2022-09-08 20:36:50 -0500

    Tighter ;)

--- src/components/Github/index.jsx
+++ src/components/Github/index.jsx
@@ -1,18 +1,20 @@
-import { h } from 'preact';
-import Styles from './styles.module.scss';
+import { h } from "preact";
+import Styles from "./styles.module.scss";
 
-const Github = ({src = null, height = null, display = true}) => {
+const GithubIFrameComponent = ({ src = null, height = null, display = true }) => {
+  if (display)
+    return (
+      <div className={Styles.github}>
+        <script
+          src={
+            `https://kbve.com/embed/github/embed.js?target=https://github.com/kbve/kbve.com/blob/main/public/` +
+            src +
+            `&style=dark&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on`
+          }
+        ></script>
+      </div>
+    );
+  else return <div>ඞ</div>;
+};
 
-    if(display)
-	return (
-        <div className={Styles.github}>
-            <script src={`https://kbve.com/embed/github/embed.js?target=https://github.com/kbve/kbve.com/blob/main/public/` + src + `&style=dark&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on`}></script>
-        </div>
-
-        );
-    else
-        return (<div>ඞ</div>);
-
-}
-
-export default Github;
+export default GithubIFrameComponent;
