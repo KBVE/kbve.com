dbd5b6f973dea3737ee206cba5c4d7df6151f149
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-09-16 01:25:39 -0400

    Updates to the Video

--- src/components/Aads/index.jsx
+++ src/components/Aads/index.jsx
@@ -1,12 +1,18 @@
 import { h } from 'preact';
-const Aads = ({width = null, height = null, display = true}) => {
+const Aads = ({width = null, height = null, display = true, adaptive = true, square = false}) => {
 
-    if(display)
+    if(display && square)
+    (
+        <div>
+            <iframe data-aa='2082301' loading='lazy' src='//ad.a-ads.com/2082301?size=250x250' style='width:250px; height:250px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+        </div>
+    );
+
+    if(display && adaptive)
 	return (
         <div>
-			<iframe data-aa='1800029' src='//ad.a-ads.com/1800029?size=728x90' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+        <iframe data-aa='2082299' loading='lazy' src='//acceptable.a-ads.com/2082299?size=Adaptive&background_color=b39797&text_color=7b3d3d&title_color=642727&title_hover_color=733030&link_color=400c0c&link_hover_color=eebdbd' style='border:0px; padding:0; width:100%; height:100%; overflow:hidden; background-color: transparent;'></iframe>
         </div>
-
         );
     else
         return (<div>ඞ</div>);
--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -2,26 +2,38 @@ import { h } from 'preact';
 import Styles from "./styles.module.scss";
 // Place-holder for now.
 
-const VideoComponent = ({ src = null, id = null, description=null, display = true }) => {
+const VideoComponent = ({ src = null, id = null, description=null, display = true, header = false}) => {
+    if (header)
+    return (
+          <>
+          <script type="module" src="https://cdn.jsdelivr.net/npm/@vidstack/player@next/cdn/bundle.js">
+          </script>
+          
+          </>
+    )
+  
     if (display)
       return (
         <div className={Styles.github}>
           {description}
 
-          <script src="https://cdn.plyr.io/3.7.2/plyr.polyfilled.js">  
-          </script>
-          
-          <link rel="stylesheet" href="https://cdn.plyr.io/3.7.2/plyr.css" />
-
-
-          <video id="player" crossorigin playsinline controls>
-          </video>
-          <div data-type="youtube" data-video-id="m3952Ft3KCo"></div>
+        <vds-media>
+          <vds-hls poster="https://media-files.vidstack.io/poster.png">
+            <video src="https://media-files.vidstack.io/hls/index.m3u8" preload="none"></video>
+          </vds-hls>
+          <vds-play-button>
+            <div class="media-play">Play</div>
+            <div class="media-pause">Pause</div>
+          </vds-play-button>
+        </vds-media>
    
         </div>
       );
     else return <div>ඞ</div>;
   };
   
+
+
+
   export default VideoComponent;
   
