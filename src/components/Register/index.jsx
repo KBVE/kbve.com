10df4c8c41d5fa84f6bd0263cf09eb4bcc44d72c
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-09-26 20:22:15 -0400

    Sync
    [1] Deprecated older docker compose
    [2] Started Register Component.

---
+++ src/components/Register/index.jsx
@@ -0,0 +1,15 @@
+import { h } from 'preact';
+const Register = ({url = "https://api.kbve.com/api/auth/local/register", display = true}) => {
+    if(display)
+	return (
+        <div>
+            
+        </div>
+
+        );
+    else
+        return (<div>ඞ</div>);
+
+}
+
+export default Register;
