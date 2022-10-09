457082131a0e67ebdc4a9cdaa029785dabdc0b24
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-10-09 23:24:15 +0000

    Update gh-pages to output generated at e1ae7c4

---
+++ index.b1cbe58d.js
@@ -0,0 +1,2 @@
+import"./chunks/index.0d03162d.js";import{u as t}from"./chunks/useCookies.35608aca.js";//! IMPORT -> [START]
+function l(){const[o,e,s]=t(["member"]);new Promise(((o,e)=>{s("user")})).then(window.location="https://kbve.com/login")}export{l as default};
