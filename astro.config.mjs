666ed85409e553c5b3684480b99eb58fb58c7382
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-10-27 17:29:38 -0400

    Test Pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import compress from "astro-compress";
-
 import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,24 +15,26 @@ export default defineConfig({
   markdown: {
     extendDefaultPlugins: true
   },
-  
   integrations: [
-    
   // preact({
   //   compat: true
   // }), 
-  
+
   sitemap({
     customPages: ["https://app.kbve.com/#/", "https://app.kbve.com/#/asset/"]
-  }), mdx(), react(), compress(), prefetch({
+  }), mdx(), react(), 
+  //compress(), 
+  prefetch({
     throttle: 20
-  })],
+  }), tailwind()],
+  
+  // Vite
+  
+  vite: {
+    ssr: {
+      //external: ["@11ty/eleventy-img", "svgo"],
+      external: ["@11ty/eleventy-img"],
 
-  // vite: {
-  //   ssr: {
-  //     noExternal: ['the-react-library'],
-  //   },}
-
-
+    },
+    }
 });
-
