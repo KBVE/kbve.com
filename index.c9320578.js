6a13d4db7d947f70b805186fd777067053384ec3
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-11-09 00:48:43 +0000

    Update gh-pages to output generated at 11b0ad7

--- index.c9320578.js
+++
@@ -1,3 +0,0 @@
-import"./chunks/index.0d03162d.js";import{u as t}from"./chunks/useCookies.35608aca.js";//! IMPORT -> [START]
-//! IMPORT -> [END]
-function r(){const[o,i,e]=t(["user"]);(()=>{e("user",{domain:".kbve.com",path:"/"}),o.user||(window.location="https://kbve.com/login")})()}export{r as default};
