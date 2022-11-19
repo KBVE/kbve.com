76ba99420233b1f8da59ad21dffc91fcbaf78f7f
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-11-19 15:20:39 -0500

    Update journalmd.json.js

--- src/pages/journal/roguejester/journalmd.json.js
+++ src/pages/journal/roguejester/journalmd.json.js
@@ -1,9 +1,16 @@
+// Import {journal}->{entry} as Glob
 const postImportResult = import.meta.glob('./_journal/*.md', { eager: true });
 
+// Sort {journal}->{posts}
 let posts = Object.values(postImportResult);
 posts.sort((a, b) => Date.parse(b.frontmatter.date) - Date.parse(a.frontmatter.date));
 
+//? Function is kept as a reference, can be removed in the future.
+function jsonFormat(str)  {
+  return str.replace(/\n/g, "\\\\n").replace(/\r/g, "\\\\r").replace(/\t/g, "\\\\t");
+}
 
+//* Export {posts} as JSON
 export const get = async () => {
   const json = JSON.stringify(
   posts.map((p) => {
@@ -13,7 +20,7 @@ export const get = async () => {
         slug: `https://kbve.com${p.url}`,
         tag: p.frontmatter.tags,
         headings: p.headings,
-        content: p.rawContent(),
+        content: jsonFormat(p.rawContent()),
       };
     })
   );
