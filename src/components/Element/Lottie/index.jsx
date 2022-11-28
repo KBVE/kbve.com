557c7e4779ff473d0e572588e162e25382ee22fe
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-11-28 01:54:49 -0500

    Lottie Interactive

--- src/components/Element/Lottie/index.jsx
+++ src/components/Element/Lottie/index.jsx
@@ -1,10 +1,12 @@
+import React from 'react';
 import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
-const Lottie = ({id = null, json = null, autoplay = true, _kbve_style = false, width = null, height = null, styler = null, ctl = false}) => {
+const Lottie = ({id = null,ref = null, json = null, autoplay = true, _kbve_style = false, width = null, height = null, styler = null, ctl = false}) => {
 
     return (<>
         <Player
     id={id}
+    ref={ref}
     autoplay={autoplay}
     loop={true}
     src={json}
