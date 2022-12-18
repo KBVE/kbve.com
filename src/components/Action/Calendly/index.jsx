8150042c73ff6a659443550cbdec3b3cea109a2a
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-12-18 17:40:03 -0500

    Support Page Updated
    [1] Fixed the Calendar Links
    [2] Added A Ticket Form
    [3] Form Submissions Page is still broken

--- src/components/Action/Calendly/index.jsx
+++ src/components/Action/Calendly/index.jsx
@@ -1,21 +1,22 @@
+import { Hidden } from '@mui/material';
 import { h } from 'preact';
-const Calendly = ({user = 'kbve', width = 320, height = 630, display = true, hide = 0, text_color="", primary_color=""}) => {
+const Calendly = ({user = 'kbve', width = 320, height = 700, display = true, hide = 0, text_color="", primary_color="", className = ""}) => {
 
     if(display)
 	return (
-            <div>
-                <div class="calendly-inline-widget" data-url={
-                    `https://calendly.com/`
-                    + user +
-                    `?hide_landing_page_details=`+hide+`&hide_gdpr_banner=`+hide+`&text_color=`+text_color+`&primary_color=`+primary_color
+        <>
+            <div className={className} data-url={
+                    `https://calendly.com/${user}?hide_landing_page_details=${hide}&hide_gdpr_banner=${hide}&text_color=${text_color}&primary_color=${primary_color}`
                 } 
                     style={{
                         minWidth: `${width}px`,
-                        height: `${height}px`
+                        height: `${height}px`,
+                        overflow: 'hidden',
+                        
                     }}
-                    ></div>
-                <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-            </div>
+                    />
+                <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async />
+         </>  
 
             );
     else
--- src/pages/project/project.json.js
+++ src/pages/project/project.json.js
@@ -1,15 +1,14 @@
 const postImportResult = import.meta.glob("./**/**/**/*.mdx", { eager: true });
 const posts = Object.values(postImportResult);
-var counterId = 0;
 
 export const get = async () => {
   const json = JSON.stringify(
     posts.map((p) => {
-      counterId++;
       return {
-        id: counterId,
+        id: p.frontmatter.id,
         title: p.frontmatter.title,
         description: p.frontmatter.description,
+        date: p.frontmatter.date,
         slug: `https://kbve.com${p.url}`,
         tag: p.frontmatter.tags,
         tags: p.frontmatter.tags,
