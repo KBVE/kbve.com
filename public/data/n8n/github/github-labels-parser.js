ee8eec84981bf00133359ce08e6b28ea25068298
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-03-06 15:54:37 +0530

    Github Updates
    - Removed default assignees
    - Backed up the Github Label Parser

--- public/data/n8n/github/github-labels-parser.js
+++ public/data/n8n/github/github-labels-parser.js
@@ -5,10 +5,19 @@
 //TODO [2] .forEach grab all the exisiting labels and consolidate them into one.
 //TODO [3] Insert custom label into the new label array.
 
+
 for (const item of $input.all()) {
     if(item.json.labels)
     {
      item.json.labelBool = true;
+      let _newLabel = 'n8n';
+      // HardCode Custom Label
+      _newLabel = _newLabel.concat(", ", "hubspot");
+      for (const label of item.json.labels)
+        {
+          _newLabel = _newLabel.concat(", ", label.name);
+        }
+      item.json.labelCollection = _newLabel;
     } else
     {
     item.json.labelBool = false;
