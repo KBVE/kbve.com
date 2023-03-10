92570ff66942874ea312b567e3f5f9db5fdb05ee
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-03-10 16:37:12 +0530

    Snapshot Sync

--- src/components/Action/Search/search.jsx
+++ src/components/Action/Search/search.jsx
@@ -6,19 +6,20 @@ import DOMPurify from "dompurify";
 const SearchData = ({}) => {
   //TODO  Sanitize the input data.
   //*     var clean = DOMPurify.sanitize(dirty, {USE_PROFILES: {html: false, mathMl: false, svg: false}});
+  //TODO  Tab System for the different search engines and have extra data points.
   const url = new URL(window.location.href);
   const searchParams = url.searchParams;
   let clean = DOMPurify.sanitize(searchParams.get("q"), {
     USE_PROFILES: { html: false, mathMl: false, svg: false },
   });
   console.log(clean);
-  if (clean) {
+  //if (clean) {
     return (
       <div>
         <div className="gcse-search"></div>
       </div>
     );
-  }
+  //}
 };
 
 export default SearchData;
