71303da4e1e995032bb9c48fbd948d3917bc3dbe
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-03-14 08:01:10 +0000

    Update gh-pages to output generated at 89f8233

--- _astro/search.6f66fefc.js
+++
@@ -1,2 +0,0 @@
-import{p as r}from"./purify.es.76a5efca.js";import{j as a}from"./jsx-runtime.1f80f1dd.js";import"./index.254df4da.js";//!     KBVE Search Module
-const i=({})=>{const s=new URL(window.location.href).searchParams;let e=r.sanitize(s.get("q"),{USE_PROFILES:{html:!1,mathMl:!1,svg:!1}});if(console.log(e),e)return a.jsx("div",{children:a.jsx("div",{className:"gcse-search"})})};export{i as default};
