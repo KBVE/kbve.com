1caa75ab8630f0f420ffb2556fb507b94d62be70
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-03-25 21:10:52 +0000

    Update gh-pages to output generated at 5c7b8ea

--- _astro/search.73cf5c49.js
+++
@@ -1,2 +0,0 @@
-import{p as r}from"./purify.es.76a5efca.js";import{j as s}from"./jsx-runtime.1f80f1dd.js";import"./index.254df4da.js";//!     KBVE Search Module
-const i=({})=>{const e=new URL(window.location.href).searchParams;let a=r.sanitize(e.get("q"),{USE_PROFILES:{html:!1,mathMl:!1,svg:!1}});return console.log(a),s.jsx("div",{children:s.jsx("div",{className:"gcse-search"})})};export{i as default};
---
+++ _astro/search.b8e340d5.js
@@ -0,0 +1,2 @@
+import{p as r}from"./purify.es.7357be35.js";import{j as s}from"./jsx-runtime.1f80f1dd.js";import"./index.254df4da.js";//!     KBVE Search Module
+const i=({})=>{const e=new URL(window.location.href).searchParams;let i=r.sanitize(e.get("q"),{USE_PROFILES:{html:!1,mathMl:!1,svg:!1}});return console.log(i),s.jsx("div",{children:s.jsx("div",{className:"gcse-search"})})};export{i as default};
