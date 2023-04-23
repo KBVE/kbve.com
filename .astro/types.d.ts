1519178270ddabf3b4f6e0bb140d23423974ecb6
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-04-23 19:17:11 +0530

    Updates
    - Started Profile Issue: https://github.com/KBVE/kbve.com/issues/183
    - Still need to update UI/UX for Login: https://github.com/KBVE/kbve.com/issues/123
    - Finished up Register: https://github.com/KBVE/kbve.com/issues/122

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -500,6 +500,13 @@ declare module 'astro:content' {
   collection: "journal",
   data: InferEntrySchema<"journal">
 } & { render(): Render[".md"] },
+"2023-04-23.md": {
+  id: "2023-04-23.md",
+  slug: "2023-04-23",
+  body: string,
+  collection: "journal",
+  data: InferEntrySchema<"journal">
+} & { render(): Render[".md"] },
 "asset.mdx": {
   id: "asset.mdx",
   slug: "asset",
