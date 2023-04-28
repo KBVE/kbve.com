249e63aaf9ffbf5f00cf6573f498ac44cffcee09
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-04-28 13:13:08 +0000

    Update gh-pages to output generated at 81dac44

--- _astro/Logout.4dc2dd55.js
+++
@@ -1,3 +0,0 @@
-import"./index.c6e42af6.js";import{u as t}from"./useCookies.0c6bab74.js";import"./_commonjsHelpers.87174ba5.js";//! IMPORT -> [START]
-//! IMPORT -> [END]
-function n(){const[o,s,e]=t(["user"]);e("user",{domain:".kbve.com",path:"/"}),o.user||(window.location="https://kbve.com/login")}export{n as default};
---
+++ _astro/Logout.51c7aed8.js
@@ -0,0 +1,3 @@
+import"./index.39e0ea27.js";import{u as t}from"./useCookies.4e3fc4fd.js";import"./_commonjsHelpers.87174ba5.js";//! IMPORT -> [START]
+//! IMPORT -> [END]
+function n(){const[o,e,s]=t(["user"]);s("user",{domain:".kbve.com",path:"/"}),o.user||(window.location="https://kbve.com/login")}export{n as default};
