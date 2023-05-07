489c1861ed5881e439fbad18299e8685e5ad298c
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-05-07 14:58:04 +0000

    Update gh-pages to output generated at 3543855

--- _astro/SHash.67fff35e.js
+++ _astro/SHash.67fff35e.js
@@ -1,3 +1,3 @@
-import{p as e}from"./purify.es.07c9e950.js";//!     Search Hash Module
+import{p as e}from"./purify.es.0c4e1f83.js";//!     Search Hash Module
 const o=({})=>{//!     There might be security issues, so I will only keep this light.
 const o=new URL(window.location.href).hash;let t=e.sanitize(o,{USE_PROFILES:{html:!1,mathMl:!1,svg:!1}});t&&(t=t.slice(1),window.location=`https://kbve.com/search/?q=${t}`)};export{o as default};
