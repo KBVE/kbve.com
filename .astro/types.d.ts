8d93662beca813459f41494c06a8f70986b7c429
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-05-18 12:34:51 -0400

    Minor updates and sync.
    - Might have to remove the search.json file and have both the search and tag index be built outside of current build? Maybe have a pre-built or expand sync, such that I do not have to recompile those files every time there is an update.

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -654,6 +654,13 @@ declare module 'astro:content' {
   collection: "journal",
   data: InferEntrySchema<"journal">
 } & { render(): Render[".md"] },
+"2023-05-18.md": {
+  id: "2023-05-18.md",
+  slug: "2023-05-18",
+  body: string,
+  collection: "journal",
+  data: InferEntrySchema<"journal">
+} & { render(): Render[".md"] },
 "asset.mdx": {
   id: "asset.mdx",
   slug: "asset",
--- src/pages/search.json.js
+++ src/pages/search.json.js
@@ -1,22 +1,23 @@
-const postImportResult = import.meta.glob('../content/**/**/*.mdx', { eager: true });
+const postImportResult = import.meta.glob("../content/**/**/*.mdx", {
+  eager: true,
+});
 const posts = Object.values(postImportResult);
 let counterId = 0;
 
-
 export const get = async () => {
   const json = JSON.stringify(
-  posts.map((p) => {
-    counterId++;
+    posts.map((p) => {
+      counterId++;
       return {
         id: counterId,
         title: p.frontmatter.title,
         body: p.frontmatter.description,
         slug: `https://kbve.com${p.url}`,
-        tag: p.frontmatter.tags
+        tag: p.frontmatter.tags,
       };
     })
   );
   return {
-    body: json
-  }
-}
+    body: json,
+  };
+};
