2eb45b5847139404b0d0239a0d5466e0dd7a9780
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-05-18 22:35:57 -0400

    Minor Sync
    Co-authored-by: BigMan1392 <BigMan1392@users.noreply.github.com>
    Co-authored-by: J. Zane Cook <jzanecook@users.noreply.github.com>

--- src/components/Widget/Search.jsx
+++ src/components/Widget/Search.jsx
@@ -7,18 +7,16 @@ import { useEffect, useState } from "react";
 //!     [Function]
 //?     Both functions below are from Gibolt to address the _blank target vulnerability.
 const onClickUrl = (url) => {
-  return () => openInNewTab(url)
-}
+  return () => openInNewTab(url);
+};
 const openInNewTab = (url) => {
-  const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
-  if (newWindow) newWindow.opener = null
-}
+  const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+  if (newWindow) newWindow.opener = null;
+};
 
-
-const SearchData = ({dork = ""}) => {
-
+const SearchData = ({ dork = "" }) => {
   //TODO  Under settings add more dorks for Google, to refine and make searching easier.
-  
+
   //*     [DATA]
   const [query, setQuery] = useState(null);
   //const [se, setEngine] = useState(engine);
@@ -36,22 +34,72 @@ const SearchData = ({dork = ""}) => {
     sanitizeData();
   }, []);
 
-  
-
   if (query) {
     return (
       <>
-      <div className="space-x-2">
-        <button type="button" className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800" onClick={onClickUrl(`https://kbve.com/search?q=${query}+site:reddit.com`)}>+Reddit</button>
-        <button type="button" className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800" onClick={onClickUrl(`https://kbve.com/search?q=${query}+site:stackoverflow.com`)}>+SOF</button>
-        <button type="button" className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800" onClick={onClickUrl(`https://kbve.com/search?q=${query}+site:kbve.com`)}>+KBVE</button>
-        <button type="button" className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800" onClick={onClickUrl(`https://duckduckgo.com/?q=${query}&kp=-1&kl=us-en`)}>DDG</button>
-        <button type="button" className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800" onClick={onClickUrl(`https://you.com/search?q=${query}&tbm=youchat&cfr=chat`)}>YOU</button>
-        <button type="button" className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800" onClick={onClickUrl(`http://ecosia.org/search.php?q=${query}`)}>Ecosia</button>
-        <button type="button" className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800" onClick={onClickUrl(`https://yep.com/web?q=${query}`)}>Yep</button>
-      </div>
+        <div className="space-x-2">
+          <button
+            type="button"
+            className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800"
+            onClick={onClickUrl(
+              `https://kbve.com/search?q=${query}+site:reddit.com`
+            )}
+          >
+            +Reddit
+          </button>
+          <button
+            type="button"
+            className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800"
+            onClick={onClickUrl(
+              `https://kbve.com/search?q=${query}+site:stackoverflow.com`
+            )}
+          >
+            +SOF
+          </button>
+          <button
+            type="button"
+            className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800"
+            onClick={onClickUrl(
+              `https://kbve.com/search?q=${query}+site:kbve.com`
+            )}
+          >
+            +KBVE
+          </button>
+          <button
+            type="button"
+            className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800"
+            onClick={onClickUrl(
+              `https://duckduckgo.com/?q=${query}&kp=-1&kl=us-en`
+            )}
+          >
+            DDG
+          </button>
+          <button
+            type="button"
+            className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800"
+            onClick={onClickUrl(
+              `https://you.com/search?q=${query}&tbm=youchat&cfr=chat`
+            )}
+          >
+            YOU
+          </button>
+          <button
+            type="button"
+            className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800"
+            onClick={onClickUrl(`http://ecosia.org/search.php?q=${query}`)}
+          >
+            Ecosia
+          </button>
+          <button
+            type="button"
+            className="px-8 py-3 font-semibold rounded shadow-2xl ring-2 ring-fushsia-900/5  dark:bg-gray-100 dark:text-gray-800"
+            onClick={onClickUrl(`https://yep.com/web?q=${query}`)}
+          >
+            Yep
+          </button>
+        </div>
       </>
-    )
+    );
   }
 };
 
