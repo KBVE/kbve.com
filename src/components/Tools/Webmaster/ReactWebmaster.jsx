5f6d2718f5f7d0a6854160340905925bce47797c
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-06-15 23:29:54 -0400

    Sync

--- src/components/Tools/Webmaster/ReactWebmaster.jsx
+++ src/components/Tools/Webmaster/ReactWebmaster.jsx
@@ -1,63 +1,74 @@
 //?     Webmaster Tools to make it easier to manage your website(s).
 
-import React from "react";
-import DOMPurify from "dompurify";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import DOMPurify from 'dompurify';
 
-//* Functions Below by Kory Becker
-
+//* [START] -> Domain Functions Below by Kory Becker
 function getHostName(url) {
-  const match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-  if (
-    match != null &&
-    match.length > 2 &&
-    typeof match[2] === "string" &&
-    match[2].length > 0
-  ) {
-    return match[2];
-  } else {
-    return null;
-  }
+	const match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+	if (
+		match != null &&
+		match.length > 2 &&
+		typeof match[2] === 'string' &&
+		match[2].length > 0
+	) {
+		return match[2];
+	} else {
+		return null;
+	}
 }
 function getDomain(url) {
-  const hostName = getHostName(url);
-  let domain = hostName;
+	const hostName = getHostName(url);
+	let domain = hostName;
 
-  if (hostName != null) {
-    const parts = hostName.split(".").reverse();
+	if (hostName != null) {
+		const parts = hostName.split('.').reverse();
 
-    if (parts != null && parts.length > 1) {
-      domain = `${parts[1]}.${parts[0]}`;
+		if (parts != null && parts.length > 1) {
+			domain = `${parts[1]}.${parts[0]}`;
 
-      if (hostName.toLowerCase().indexOf(".co.uk") !== -1 && parts.length > 2) {
-        domain = `${parts[2]}.${domain}`;
-      }
-    }
-  }
+			if (hostName.toLowerCase().indexOf('.co.uk') !== -1 && parts.length > 2) {
+				domain = `${parts[2]}.${domain}`;
+			}
+		}
+	}
 
-  return domain;
+	return domain;
 }
-
+//* [END] -> Domain Functions
+///
+///
+//! [START] -> ReactWebmaster
 const ReactWebmaster = () => {
-  const [domain, setDomain] = useState(null);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 
-  useEffect(() => {
-    const sanitizeData = async () => {
-      const _url = getDomain(dork);
-      const clean = DOMPurify.sanitize(_url, {
-        USE_PROFILES: { html: false, mathMl: false, svg: false },
-      });
-      setDomain(clean);
-    };
-    sanitizeData();
-  }, []);
+	const [domain, setDomain] = React.useState(null);
+  const [webmasterError, setWebmasterError] = React.useState('');
+  
+	React.useEffect(() => {
+		const sanitizeData = async () => {
+			const _url = getDomain(dork);
+			const clean = DOMPurify.sanitize(_url, {
+				USE_PROFILES: { html: false, mathMl: false, svg: false },
+			});
+			setDomain(clean);
+		};
+		sanitizeData();
+	}, []);
 
-  if (domain) {
-    return (
-      <>
-        <div>{domain}</div>
-      </>
-    );
-  }
+	if (domain) {
+		return (
+			<>
+				<div>{domain}</div>
+			</>
+		);
+	}
 };
 
+//! [END] -> ReactWebmaster
 export default ReactWebmaster;
