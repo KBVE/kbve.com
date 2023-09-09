397542fb33967377997ad8afc38318083b4dbc94
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-09-09 12:27:11 -0400

    Updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -56,11 +56,11 @@ module.exports = {
       },
       keyframes: {
         float: {
-          '0% 100%': { transform: 'translate3d(0, 0, 0)' },
+          '0%, 100%': { transform: 'translate3d(0, 0, 0)' },
           '50%': { transform: 'translate3d(0, 30px, 0)' },
         },
         'ltr-linear-infinite': {
-          '0% 100%': { 'background-position': '0 0' },
+          '0%, 100%': { 'background-position': '0 0' },
           '50%': { 'background-position': '400% 0%' },
         },
        
