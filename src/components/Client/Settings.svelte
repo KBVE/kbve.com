ef4008e434a34388867fedf4b700fb830f903cde
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-09-21 16:32:02 -0400

    PGP Sync
    PGP Encryption Test Case

--- src/components/Client/Settings.svelte
+++ src/components/Client/Settings.svelte
@@ -2,19 +2,20 @@
     declare global {
         interface Window {
             Toastify: any;
-            Flowbite: any;
+			openpgp: any;
         }
     }
 
     declare var Toastify: any;
-    declare var Flowbite: any;
+	declare var openpgp: any;
 </script>
 
 <script lang="ts">
+	import { Textarea, Label, Input } from 'flowbite-svelte';
+
     import { onMount, onDestroy, createEventDispatcher } from 'svelte';
     import {appwriteFunctions} from "@c/API/appwrite/appwrite"; // Appwrite
-	import { log, notification$, toast$, notification, tasker } from '@c/API/storage';
-    import * as kbve from "@c/kbve"; // KBVE
+	import { log, notification$, toast$, notification, tasker, kbve$ } from '@c/API/storage';
 
     import WidgetWrapper  from './UX/WidgetWrapper.svelte';
 
@@ -25,7 +26,7 @@
     
     // [EXPORT]
     export const toast = () => {
-		if(mounted && loaded)
+		if(mounted)
 		{
 			new Toastify({
 				text: $toast$,
@@ -51,21 +52,33 @@
     // [VAR]
 
     let mounted = false;
-    let loaded = false;
+    let loading = false;
     let skeleton: any;
     let bio = '';
     let pgp = '';
+	let username = '';
+	let uuid = '';
+	let email = '';
+	let phone = '';
 
+	//	[UI]
+
+	let textAreaProps = {
+		id: 'pgp',
+		name: 'pgp',
+		label: 'Your message',
+		rows: 4,
+		placeholder: 'Please paste your public key here... Make sure its your PUBLIC key!'
+	};
 
     // []
 
-    const _updatePGP = async () => {
-        try {
+	
+	const handleSettings = async () => {
 
-        } catch (e) {
-            
-        }
-    }
+		console.log(`Submitting PGP Key: ${pgp}`);
+		
+	};
 
 
 
@@ -74,16 +87,17 @@
     onMount( () => {
         dispatch('mount');
         mounted = true;
-        //! [TEMP]@REMOVE
-        loaded = true;
     })
     
     onDestroy( () => {
 
     })
 
-    $: if(mounted && loaded) {
-
+    $: if(mounted) {
+		username = $kbve$.username;
+		uuid = $kbve$.uuid;
+		email = $kbve$.email;
+		phone = $kbve$.phone;
         skeleton = window.document.getElementById('skeleton') as HTMLElement;
         if(skeleton) skeleton.remove();
 		tasker(toast$, "Welcome to the Account Settings").then(toast);
@@ -92,30 +106,31 @@
 
 </script>
 
+<svelte:head>
+	{#if mounted && !window?.openpgp}
+		<script src={`https://github.com/openpgpjs/openpgpjs`} async defer></script>
+	{/if}
+</svelte:head>
+
 
 <WidgetWrapper
-	background="https://images.unsplash.com/photo-1609097828576-3b620e86039e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=80">
+	background="https://images.unsplash.com/photo-1609097828576-3b620e86039e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=80">
 	<section class="">
-		<div
-			class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-			<a href="/#" class="flex items-center mb-6">
-				<img
-					class="w-32 mr-2"
-					src="https://kbve.com/assets/img/letter_logo.svg"
-					alt="logo" />
-			</a>
-            <div
-				class="w-full bg-transparent rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
-				<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <div class="min-h-[100px]">
+						<div class="w-full px-3 py-16 rounded-3xl sm:px-12 md:px-16 xl:col-span-2 bg-default bg-cover bg-blend-overlay" style="background-image: url('https://kbve.com/assets/img/curved-images/wave.jpg')">
+							<h1
+									class="text-xl font-bold leading-tight tracking-tight gradient-text md:text-2xl">
+									Account Settings
+							</h1>
+						</div>
                         {#if $notification$}
-							<h1>
+							
 								<div
 									id="toast-default"
-									class="flex items-center w-full max-w-xs p-4 bg-offset rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+									class="flex items-center w-full max-w-xs p-4 bg-offset rounded-lg shadow "
 									role="alert">
 									<div
-										class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200">
+										class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg">
 										<svg
 											class="w-4 h-4"
 											aria-hidden="true"
@@ -156,21 +171,77 @@
 										</svg>
 									</button>
 								</div>
-							</h1>
+							
 						{:else}
-							<h1
-								class="text-xl font-bold leading-tight tracking-tight gradient-text md:text-2xl">
-								Register your KBVE Account!
-							</h1>
+							<div>
+
+							</div>
 						{/if}
                     </div>
-                    
-                </div>
-            </div>
-        </div>
-
-
-
     </section>
+	<section class="p-6 text-gray-50">
+		<form  action="" class="container flex flex-col mx-auto space-y-12" on:submit|preventDefault={handleSettings}>
+			<fieldset class="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-gray-900/60">
+				<div class="space-y-2 col-span-full lg:col-span-1">
+					<p class="font-medium">Account Preferences</p>
+					<p class="text-xs">The account preferences section allows users to customize their experience by managing settings related to privacy, notifications, and theme, enabling a personalized interaction with the software.</p>
+				</div>
+				<div class="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+					<div class="col-span-full sm:col-span-3">
+						<label for="username" class="text-sm">Username</label>
+						<input id="username" type="text" class="w-full rounded-md focus:ring bg-gray-500 border-gray-700 text-gray-900 cursor-not-allowed" bind:value={username} disabled>
+					</div>
+					<div class="col-span-full sm:col-span-3">
+						<label for="uuid" class="text-sm">UUID</label>
+						<input id="uuid" type="text" class="w-full rounded-md focus:ring bg-gray-500 border-gray-700 text-gray-900 cursor-not-allowed" bind:value={uuid} disabled>
+					</div>
+					<div class="col-span-full sm:col-span-3">
+						<label for="email" class="text-sm">Email</label>
+						<input id="email" type="email"  class="w-full rounded-md focus:ring bg-gray-500  border-gray-700 text-gray-900 cursor-not-allowed" bind:value={email} disabled>
+					</div>
+					<div class="col-span-full sm:col-span-3">
+						<label for="phone" class="text-sm">Phone</label>
+						<input id="phone" type="text"  class="w-full rounded-md focus:ring bg-gray-500 border-gray-700 text-gray-900 cursor-not-allowed" bind:value={phone} disabled>
+					</div>
+
+				</div>
+			</fieldset>
+			<fieldset class="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-gray-900">
+				<div class="space-y-2 col-span-full lg:col-span-1">
+					<p class="font-medium">Profile</p>
+					<p class="text-xs">The account profile serves as a public-facing overview, displaying user-provided information such as username, bio, and shared content, allowing other users to learn more about the individual and interact with them.</p>
+				</div>
+				<div class="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+					<div class="col-span-full sm:col-span-3">
+						<label for="username" class="text-sm">Username</label>
+						<input id="username" type="text" class="w-full  rounded-md focus:ring  bg-gray-500 border-gray-700 text-gray-900 cursor-not-allowed" bind:value={username} disabled>
+					</div>
+					<div class="col-span-full sm:col-span-3">
+						<label for="uuid" class="text-sm">UUID</label>
+						<input id="uuid" type="text"  class="w-full rounded-md focus:ring bg-gray-500 border-gray-700 text-gray-900 cursor-not-allowed"  bind:value={uuid} disabled>
+					</div>
+					<div class="col-span-full">
+						<label for="bio" class="text-sm">Bio</label>
+						<textarea id="bio" class="w-full rounded-md focus:ring  border-gray-700 text-gray-900" bind:value={bio}></textarea>
+					</div>
+					<div class="col-span-full">
+						<Label class="pb-2 text-sm text-white-50">PGP</Label>
+						<Textarea {...textAreaProps} bind:value={pgp} />
+					</div>
+					
+				</div>
+			</fieldset>
+			<div class="grid gap-1 md:grid-cols-3">
+				<button type="submit" class="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group" disabled={loading}>
+					<span class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+					<span class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+					<span class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+					<span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+					<span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
+					<span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Update</span>
+				</button>
+			</div>
+		</form>
+	</section>
 </WidgetWrapper>
 
