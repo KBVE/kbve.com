caa906b94a5903939b87d731fffe1c3329079a06
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2024-11-18 02:34:10 +0000

    Merge pull request #3237 from KBVE/beta
    Preparing Release Branch

--- docs/types.d.ts
+++ docs/types.d.ts
@@ -3468,6 +3468,20 @@ declare module 'astro:content' {
   collection: "journal";
   data: InferEntrySchema<"journal">
 } & { render(): Render[".mdx"] };
+"11-16.mdx": {
+	id: "11-16.mdx";
+  slug: "11-16";
+  body: string;
+  collection: "journal";
+  data: InferEntrySchema<"journal">
+} & { render(): Render[".mdx"] };
+"11-17.mdx": {
+	id: "11-17.mdx";
+  slug: "11-17";
+  body: string;
+  collection: "journal";
+  data: InferEntrySchema<"journal">
+} & { render(): Render[".mdx"] };
 };
 "legal": {
 "disclaimer.mdx": {
