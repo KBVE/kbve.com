a6496d765bdeb644b0b4943482cdcd70fcfaf952
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-05-19 11:41:30 +0000

    Merge pull request #4709 from KBVE/dev
    Preparing Main Branch

--- docs/_astro/hast-util-whitespace.D4Wp6AEg.js
+++ docs/_astro/hast-util-whitespace.D4Wp6AEg.js
@@ -1,1 +1,1 @@
-const r=/[ \t\n\f\r]/g;function c(e){return typeof e=="object"?e.type==="text"?t(e.value):!1:t(e)}function t(e){return e.replace(r,"")===""}export{c as w};
+const r=/[ \t\n\f\r]/g;function c(e){return"object"==typeof e?"text"===e.type&&t(e.value):t(e)}function t(t){return""===t.replace(r,"")}export{c as w};
