c33f8dcbca67ed9361baaae8edbfcbe9a2641b1a
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2025-05-23 23:30:56 +0000

    Update gh-pages to output generated at 4fc6d5b

--- _astro/ws.BC-UVmRa.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./@react-three.4bQWeJYP.js";var r,e;function t(){return e||(e=1,r=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}),r}var s=t();const n=o(s),a=Object.freeze(Object.defineProperty({__proto__:null,default:n},Symbol.toStringTag,{value:"Module"}));export{a as b};
